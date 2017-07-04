387e015de1f99ff78d03deef6fca4540f2f36fbc
PaperEd <dwg7691@gmail.com>
2017-07-04 09:39:40 +0900

    Get 추가
    오로지 Get만 된다 Get만

---
+++ Server_1/Server_1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleApp1
+{
+    class REST
+    {
+        public static void getUser(string url) // 유저 GET
+        {
+            string result = null;
+            string user;
+            user = Console.ReadLine();
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url + "getUser/" + user);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                result = reader.ReadToEnd();
+                stream.Close();
+                response.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                JObject obj = JObject.Parse(result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine(result);
+        }
+
+    }
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string url = "http://127.0.0.1:3000/";
+
+            REST.getUser(url);
+        }
+    }
+}
